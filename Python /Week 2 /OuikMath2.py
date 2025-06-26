09e87fd6cbbc5a6ff7a25d05c917c4889263b8f8
Taymur Dixon <taymur.dixon@hgs.hiddengeniusproject.org>
2025-06-26 13:58:49 -0700

    Exit Ticket

---
+++ Python /Week 2 /OuikMath2.py
@@ -0,0 +1,26 @@
+def Add(addend1, addend2):
+    return addend1 + addend2
+
+sum = Add(5, 35)
+print(sum)
+
+
+def Sub(subtrated1, subtracted2):
+    return subtrated1 - subtracted2
+
+difference  = Sub(304, 2)
+print(difference)
+
+
+def Div(divided1, divided2):
+    return divided1 / divided2
+
+quotient = Div(250, 4)
+print(quotient)
+
+
+def Mul(mulitpled1, mulitpled2):
+    return mulitpled1 * mulitpled2
+ 
+product = Mul(24,3)
+print(product)
